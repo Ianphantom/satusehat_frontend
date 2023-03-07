d2539180f50397ed1c41fae997dcca691291f2c4
tugasSBD <ian25yola@gmail.com>
2023-03-07 21:01:35 +0700

    EDIT : Use profile header compononet and profile section item component on profile pages

--- src/pages/user/Profile.js
+++ src/pages/user/Profile.js
@@ -3,6 +3,15 @@ import styled from "styled-components";
 
 // import components
 import ProfileCardHeader from "../../components/user/ProfileCardHeader";
+import ProfileSectionItem from "../../components/user/ProfileSectionItem";
+
+// import icon
+import iconNama from "../../images/svg-icon/icon-nama.svg";
+import calendar from "../../images/svg-icon/icon-calender.svg";
+import email from "../../images/svg-icon/icon-mail.svg";
+import phone from "../../images/svg-icon/icon-phone.svg";
+import password from "../../images/svg-icon/icon-password.svg";
+import settings from "../../images/svg-icon/icon-settings.svg";
 
 const Profile = () => {
   return (
@@ -11,6 +20,45 @@ const Profile = () => {
         <ProfileCardHeader
           title='Informasi Pribadi'
           desc='Data yang umumnya ditampilkan pada aplikasi satu sehat'
+        />
+        <ProfileSectionItem
+          icon={iconNama}
+          title='Nama'
+          desc='Ubah nama lengkap dan nama panggilan'
+        />
+        <ProfileSectionItem
+          icon={calendar}
+          title='Tanggal Lahir'
+          desc='Melihat tanggal lahir'
+        />
+      </div>
+      <div className='card-design'>
+        <ProfileCardHeader title='Kontak' desc='Ubah email dan nomor ponsel' />
+        <ProfileSectionItem
+          icon={email}
+          title='Email'
+          desc='Perbarui dan verifikasi email'
+        />
+        <ProfileSectionItem
+          icon={phone}
+          title='Nomor Ponsel'
+          desc='Perbarui dan verifikasi nomor telepon'
+        />
+      </div>
+      <div className='card-design'>
+        <ProfileCardHeader
+          title='Informasi Pribadi'
+          desc='Data yang umumnya ditampilkan pada aplikasi satu sehat'
+        />
+        <ProfileSectionItem
+          icon={password}
+          title='Kata Sandi'
+          desc='Perbarui kata sandi'
+        />
+        <ProfileSectionItem
+          icon={settings}
+          title='Pengaturan Web'
+          desc='Sesuaikan preferensi tampilan'
         />
       </div>
     </ProfileContainer>
@@ -21,7 +69,9 @@ const ProfileContainer = styled.div`
   margin-top: 40px;
   margin-left: 20px;
   margin-bottom: 40px;
-
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
   .card-design {
     border-radius: 8px;
     background: white;
