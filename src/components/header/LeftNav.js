6573286a3ddd3a8c26d9c2e92d88c309ee06e94c
tugasSBD <ian25yola@gmail.com>
2023-03-13 15:31:36 +0700

    EDIT : CSS Style left nav

--- src/components/header/LeftNav.js
+++ src/components/header/LeftNav.js
@@ -8,14 +8,14 @@ const LeftNav = () => {
   return (
     <LeftNavContainer>
       {isLogged ? (
-        <>
+        <div className='userLoginNavigation'>
           <Link className='nav-item nav-link' to='/user/dashboard'>
             Dashboard
           </Link>
           <Link to='/logout' className='button-container'>
             Logout
           </Link>
-        </>
+        </div>
       ) : (
         <Link to='/login' className='button-container'>
           Login
@@ -38,6 +38,12 @@ const LeftNavContainer = styled.div`
     font-weight: 700;
     font-size: 14px;
     line-height: 17px;
+  }
+
+  .userLoginNavigation {
+    display: flex;
+    align-items: center;
+    gap: 30px;
   }
 `;
 
